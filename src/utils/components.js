5284a0d07470c48a26672b3c6ede2a943f5c05b2
jurelik <jure@likar.me>
2021-02-08 22:09:29 +0000

    updated play icon

--- src/utils/components.js
+++ src/utils/components.js
@@ -1,4 +1,4 @@
-const playIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="537.5 236.5 183 183"><path fill="#EEE" stroke="#EEE" stroke-width="15" stroke-linecap="round" stroke-linejoin="round" d="M554 253h0v150l150-75-150-75"/><path fill="none" d="M537.5 236.5h183v183h-183z"/></svg></svg>';
+const playIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="408.5 238.5 183 183"><path fill="#EEE" stroke="#EEE" stroke-width="15" stroke-linecap="round" stroke-linejoin="round" d="M443.75 255c-8.285 0-15 6.716-15 15h0v120c0 8.285 6.715 15 15 15h0l120-60c10-10 10-20 0-30h0l-120-60"/><path fill="none" d="M408.5 238.5h183v183h-183z"/></svg>';
 const pauseIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="534.5 238.5 183 183"><path fill="#EEE" stroke="#EEE" stroke-width="15" stroke-linecap="round" stroke-linejoin="round" d="M566 255h0v150h30V255h-30m120 0h0-30v150h30V255"/><path fill="none" d="M534.5 238.5h183v183h-183z"/></svg>'
 
 const createSongElement = (_song) => {
