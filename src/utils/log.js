16aef13f5ddda1a5d2637b60f72316e2ca4cc78f
jurelik <jure@likar.me>
2021-07-09 21:57:40 +0100

    fixed log.js

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,5 +1,5 @@
 const _log = console.log.bind(window.console, '%c%s', 'margin-top: 5px;');
-const success = console.log.bind(window.console, '%c%s ✅', 'color:#00c30f;');
+const success = console.log.bind(window.console, '%c%s', 'color:#00c30f; margin-top: 5px;');
 const error = console.error.bind(window.console);
 const warn = console.warn.bind(window.console);
 
