4605810477bda80aecb5e3b8d72f7fe65258b1e9
jurelik <jure@likar.me>
2021-08-02 20:58:30 +0100

    settingsView is now functional, settings change when clicking save button

--- src/views/settingsView.js
+++ src/views/settingsView.js
@@ -7,6 +7,18 @@ function SettingsView(data) {
   this.data = data;
   this.settings = app.settingsStore.get();
 
+  this.handleSave = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    let settings = {};
+    const query = this.el.querySelectorAll('div[contenteditable="true"]')
+    for (const setting of query) settings[setting.id] = setting.textContent;
+
+    app.settingsStore.set(settings);
+    log.success('Settings successfully changed.');
+  }
+
   this.createSetting = (name, value) => {
     //Create elements
     const el = document.createElement('div');
@@ -19,6 +31,7 @@ function SettingsView(data) {
     //Add attributes and innerHTML/textContent
     _name.textContent = name + ': ';
     _value.textContent = value;
+    _value.setAttribute('id', `${name}`);
     _value.setAttribute('contenteditable', 'true');
 
     //Build structure
@@ -30,19 +43,25 @@ function SettingsView(data) {
 
   this.render = () => {
     //Create elements
+    const save = document.createElement('input');
 
     //Add classes for styling
     this.el.className = 'settings-view'
+    save.className = 'save';
 
     //Add attributes and innerHTML/textContent
+    save.setAttribute('type', 'submit');
+    save.setAttribute('value', 'save');
 
     //Build structure
     for (const name in this.settings) {
       const el = this.createSetting(name, this.settings[name]);
       this.el.appendChild(el);
     }
+    this.el.appendChild(save);
 
     //Add listeners
+    save.addEventListener('click', this.handleSave);
 
     app.content.innerHTML = '';
     app.content.appendChild(this.el);
