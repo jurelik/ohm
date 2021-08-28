e120138db88f16f65ea76f915a240d867952a3de
jurelik <jure@likar.me>
2021-08-28 17:52:16 +0100

    added more console log breakpoints

--- src/app.js
+++ src/app.js
@@ -117,6 +117,7 @@ function App() {
           port: this.settingsStore.getOne('IPFS_PORT'),
           apiPath: this.settingsStore.getOne('IPFS_PATH'),
         });
+        console.log('bb')
         const id = await this.getId(); //Get multiaddress for swarm connections
         this.MULTIADDR = id.addresses[4];
 
@@ -161,6 +162,7 @@ function App() {
   this.getId = async () => {
     try {
       const id = await this.ipfs.id();
+      console.log(id);
       const a = id.addresses[4].toString().split('/');
       if (a[3] !== 'tcp') return await this.getId(); //Check to see if the tcp adress is in the right position and if not, run the same function again
       return id;
