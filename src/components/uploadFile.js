544e15ac53fe7a9a185f4a0d8697d073d4193658
jurelik <jure@likar.me>
2021-09-26 16:49:10 +0100

    fixed file tags error for internal files

--- src/components/uploadFile.js
+++ src/components/uploadFile.js
@@ -124,13 +124,13 @@ function UploadFile(data) {
       if (file.path === '') throw new Error('File is missing.');
       if (file.tags === '') throw new Error('File tags are missing.');
       if (!helpers.allowedFormat(file.name)) throw new Error('File name can only include letters, numbers and underscores.'); //Check for bad characters
+
+      file.tags = file.tags.split(/[,;]+/).filter(tag => tag.length > 0); //Turn tags into an array
+      if (file.tags.length === 0) throw new Error('File tags are missing');
     }
     else if (file.type === 'internal') {
       if (file.id === '') throw 'file id is missing';
     }
-
-    file.tags = file.tags.split(/[,;]+/).filter(tag => tag.length > 0); //Turn tags into an array
-    if (file.tags.length === 0) throw new Error('File tags are missing');
 
     //Check formatting
     if (!helpers.allowedFormat(file.name)) throw new Error('File name can only include letters, numbers and underscores.'); //Check for bad characters
