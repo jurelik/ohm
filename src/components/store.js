52488b790debd1c543b3116723690506c419f737
jurelik <jure@likar.me>
2021-10-11 19:06:08 +0100

    store now stringifies JSON with spacing

--- src/components/store.js
+++ src/components/store.js
@@ -35,24 +35,24 @@ function Store(opts) {
 
   this.set = (val) => { //Set entire store
     this.data = val;
-    fs.writeFileSync(this.path, JSON.stringify(this.data));
+    fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2));
   }
 
   this.add = (unique, obj) => { //Add item to store
     this.data[unique] = obj;
-    fs.writeFileSync(this.path, JSON.stringify(this.data));
+    fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2));
 
     return this.data[unique];
   }
 
   this.rm = (unique) => { //Remove item from store
     delete this.data[unique];
-    fs.writeFileSync(this.path, JSON.stringify(this.data));
+    fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2));
   }
 
   this.update = (unique, obj, temporary) => { //Update value of items (temporary flag means the changes won't be written to disk)
     for (let key in obj) this.data[unique][key] = obj[key];
-    if (!temporary) fs.writeFileSync(this.path, JSON.stringify(this.data));
+    if (!temporary) fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2));
   }
 }
 
