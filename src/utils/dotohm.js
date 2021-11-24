ab5f866eb66dd469e51fc5040693a60d02cdcdd8
jurelik <jure@likar.me>
2021-11-24 17:51:14 +0000

    started working on the dotohm generate function

--- src/utils/dotohm.js
+++ src/utils/dotohm.js
@@ -2,7 +2,28 @@
 
 const generate = async (path, payload) => {
   try {
-    await app.ipfs.files.write(path, JSON.stringify(payload, null, 2), { create: true, cidVersion: 1 });
+    const _payload = JSON.parse(JSON.stringify(payload)); //Make a copy of the payload so that the original does not get modified
+
+    //Delete unnecessary properties from payload/songs/files
+    delete _payload.multiaddr;
+    for (let song of _payload.songs) {
+      delete song.path;
+      for (let file of song.files) delete file.path;
+    }
+
+    const data =`This is an automatically generated file which contains metadata related to an ohm upload.
+
+Copy the text below to give credit to artists whose files are included in this upload:
+=====================================================================================
+
+This work includes previous work of the following artists:
+
+
+Copy the text below into an .ous file in order to re-create this upload and re-upload:
+=====================================================================================
+${_payload}`
+
+    await app.ipfs.files.write(path, JSON.stringify(data, null, 2), { create: true, cidVersion: 1 });
   }
   catch (err) {
     throw err;
@@ -23,4 +44,5 @@ const getOus = (e) => {
 }
 
 module.exports = {
+  generate
 }
